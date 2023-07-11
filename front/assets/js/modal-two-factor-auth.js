9ef3569fba3402a26306b4681b7694e56ae56850
Nexeum <juaandhe@bancolombia.com.co>
2023-07-10 20:34:33 -0500

    Update files

--- front/assets/js/modal-two-factor-auth.js
+++
@@ -1,1 +0,0 @@
-"use strict";document.addEventListener("DOMContentLoaded",function(e){var n;(n=document.querySelectorAll("#twoFactorAuthInputSms"))&&n.forEach(function(e){new Cleave(e,{phone:!0,phoneRegionCode:"US"})})});
