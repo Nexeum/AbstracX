923ab86bcb6f676d9ff3f966fce965f5fcb68a75
Nexeum <srandres1803@gmail.com>
2023-08-04 17:03:18 -0400

    Register config

--- sys/register.php
+++ sys/register.php
@@ -9,7 +9,7 @@ $link = mysqli_connect("localhost", "root", "", "nexeum");
         if ($("input[name='reg_teamname']").val() == "") {
             str += "Team Name not provided.\n";
         }
-        if ($("input[name='reg_teamname']").val().match(/<?php echo preg_replace("\n", "\\n", $invalidchars); ?>/i) != null) {
+        if ($("input[name='reg_teamname']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/i) != null) {
             str += "Team Name contains invalid characters.\n";
         }
         if ($("input[name='reg_pass1']").val() == "") {
@@ -34,25 +34,51 @@ $link = mysqli_connect("localhost", "root", "", "nexeum");
             str += "Phone-Number-1 not provided.\n";
         }
 
-
-
-        if ($("input[name='reg_name1']").val().match(/<?php echo preg_replace("\n", "\\n", $invalidchars); ?>/i) != null) str += "Name-1 contains invalid characters.\n";
-        if ($("input[name='reg_roll1']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Roll-Number-1 contains invalid characters.\n";
-        if ($("input[name='reg_branch1']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Branch-1 contains invalid characters.\n";
-        if ($("input[name='reg_email1']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "EMail-Address-1 contains invalid characters.\n";
-        if ($("input[name='reg_phone1']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Phone-1 contains invalid characters.\n";
-
-        if ($("input[name='reg_name2']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Name-2 contains invalid characters.\n";
-        if ($("input[name='reg_roll2']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Roll-Number-2 contains invalid characters.\n";
-        if ($("input[name='reg_branch2']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Branch-2 contains invalid characters.\n";
-        if ($("input[name='reg_email2']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "EMail-Address-2 contains invalid characters.\n";
-        if ($("input[name='reg_phone2']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Phone-2 contains invalid characters.\n";
-
-        if ($("input[name='reg_name3']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Name-3 contains invalid characters.\n";
-        if ($("input[name='reg_roll3']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Roll-Number-3 contains invalid characters.\n";
-        if ($("input[name='reg_branch3']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Branch-3 contains invalid characters.\n";
-        if ($("input[name='reg_email3']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "EMail-Address-3 contains invalid characters.\n";
-        if ($("input[name='reg_phone3']").val().match(/<?php echo preg_replace("\n","\\n",$invalidchars); ?>/) != null) str += "Phone-3 contains invalid characters.\n";
+        if ($("input[name='reg_name1']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/i) != null) {
+            str += "Name-1 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_roll1']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Roll-Number-1 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_branch1']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Branch-1 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_email1']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "EMail-Address-1 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_phone1']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Phone-1 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_name2']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Name-2 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_roll2']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Roll-Number-2 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_branch2']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Branch-2 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_email2']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "EMail-Address-2 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_phone2']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Phone-2 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_name3']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Name-3 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_roll3']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Roll-Number-3 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_branch3']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Branch-3 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_email3']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "EMail-Address-3 contains invalid characters.\n";
+        }
+        if ($("input[name='reg_phone3']").val().match(/<?php echo preg_replace("/\n/", "\\n", $invalidchars); ?>/) != null) {
+            str += "Phone-3 contains invalid characters.\n";
+        }
         if (str == "") {
             return true;
         }
@@ -60,92 +86,98 @@ $link = mysqli_connect("localhost", "root", "", "nexeum");
         return false;
     }
 </script>
-    <form action='?action=register' method='post' onsubmit="return validate_register()">
-        <h2>Team Registeration</h2>
-        <table>
-            <tr>
-                <th colspan>Team Information (Compulsary)</th>
-                <td class='vdiv'></td>
-                <th colspan>Team Member 1 (Compulsary)</th>
-            </tr>
-            <tr>
-                <td>Team Name</td>
-                <td><inputname='reg_teamname'></td>
-                <td>Full Name</td>
-                <td><input name='reg_name1'></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td><input name='reg_pass1' type='password'></td>
-                <td>Roll Number</td>
-                <td><input name='reg_roll1'></td>
-            </tr>
-            <tr>
-                <td>Retype Password</td>
-                <td><input name='reg_pass2' type='password'></td>
-                <td>Branch</td>
-                <td><input name='reg_branch1'></td>
-            </tr>
-            <tr>
-                <td><span>All values (except for the Password) must be a combination of upto 30 characters characters (single and double quotes are not allowed).</span>
-                </td>
-                <td>Email Address</td>
-                <td><input name='reg_email1'></td>
-            </tr>
-            <tr>
-                <td>Team Group</td>
-                <td>
-                    <select name='reg_gid'>
-                        <?php
-	                        $data = mysqli_query($link,"SELECT * FROM groups WHERE statusx<3;");
-	                        while($row = mysqli_fetch_array($data)) {
-                                echo "<option value=$row[gid]>$row[groupname]</option>";
-                            }
+<form action='?action=register' method='post' onsubmit="return validate_register()">
+    <table class="table table-borderless">
+        <tr class="table-primary">
+            <th colspan="4">
+                <h3>Team Registeration</h3>
+            </th>
+        </tr>
+        <tr>
+            <th class="table-info">Team Information (Compulsary)</th>
+            <td class='vdiv'></td>
+            <th class="table-info">Team Member 1 (Compulsary)</th>
+        </tr>
+        <tr>
+            <td class="table-info">Team Name</td>
+            <td>
+                <input class="form-control" name='reg_teamname'>
+            </td>
+            <td class="table-info">Full Name</td>
+            <td><input class="form-control"  name='reg_name1'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Password</td>
+            <td><input class="form-control"  name='reg_pass1' type='password'></td>
+            <td class="table-info">Roll Number</td>
+            <td><input class="form-control"  name='reg_roll1'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Retype Password</td>
+            <td><input class="form-control" class="form-control"  name='reg_pass2' type='password'></td>
+            <td class="table-info">Branch</td>
+            <td><input class="form-control"  name='reg_branch1'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Team Group</td>
+            <td>
+                <select class="form-select" name='reg_gid'>
+                    <?php
+                    $data = mysqli_query($link, "SELECT * FROM groups WHERE statusx<3;");
+                    while ($row = mysqli_fetch_array($data)) {
+                        echo "<option value=$row[gid]>$row[groupname]</option>";
+                    }
                     ?>
-                    </select>
-                </td>
-                <td>Phone Number</td>
-                <td><input name='reg_phone1'></td>
-            </tr>
-            <tr>
-                <td class='hdiv'></td>
-            </tr>
-            <tr>
-                <th>Team Member 2 (Optional)</th>
-                <td class='vdiv'></td>
-                <th>Team Member 3 (Optional)</th>
-            </tr>
-            <tr>
-                <td>Full Name</td>
-                <td><input name='reg_name2'></td>
-                <td>Full Name</td>
-                <td><input name='reg_name3'></td>
-            </tr>
-            <tr>
-                <td>Roll Number</td>
-                <td><input name='reg_roll2'></td>
-                <td>Roll Number</td>
-                <td><input name='reg_roll3'></td>
-            </tr>
-            <tr>
-                <td>Branch</td>
-                <td><input name='reg_branch2'></td>
-                <td>Branch</td>
-                <td><input name='reg_branch3'></td>
-            </tr>
-            <tr>
-                <td>EMail Address</td>
-                <td><input name='reg_email2'></td>
-                <td>EMail Address</td>
-                <td><input name='reg_email3'></td>
-            </tr>
-            <tr>
-                <td>Phone Number</td>
-                <td><input name='reg_phone2'></td>
-                <td>Phone Number</td>
-                <td><input name='reg_phone3'></td>
-            </tr>
-        </table>
-        <br>
-        <input type='submit' value='Submit Data'>
+                </select>
+            </td>
+            <td class="table-info">Email Address</td>
+            <td><input class="form-control" name='reg_email1'></td>
+        </tr>
+        <tr>
+            <td colspan="2"><span>All values (except for the Password) must be a combination of upto 30 characters characters (single and double quotes are not allowed).</span>
+            </td>
+            <td class="table-info">Phone Number</td>
+            <td><input class="form-control"  name='reg_phone1'></td>
+        </tr>
+        <tr>
+            <td class='hdiv'></td>
+        </tr>
+        <tr>
+            <th class="table-info">Team Member 2 (Optional)</th>
+            <td class='vdiv'></td>
+            <th class="table-info">Team Member 3 (Optional)</th>
+        </tr>
+        <tr>
+            <td class="table-info">Full Name</td>
+            <td><input class="form-control" name='reg_name2'></td>
+            <td class="table-info">Full Name</td>
+            <td><input class="form-control" name='reg_name3'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Roll Number</td>
+            <td><input class="form-control" name='reg_roll2'></td>
+            <td class="table-info">Roll Number</td>
+            <td><input class="form-control" name='reg_roll3'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Branch</td>
+            <td><input class="form-control" name='reg_branch2'></td>
+            <td class="table-info">Branch</td>
+            <td><input class="form-control" name='reg_branch3'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Email Address</td>
+            <td><input class="form-control" name='reg_email2'></td>
+            <td class="table-info">Email Address</td>
+            <td><input class="form-control" name='reg_email3'></td>
+        </tr>
+        <tr>
+            <td class="table-info">Phone Number</td>
+            <td><input class="form-control" name='reg_phone2'></td>
+            <td class="table-info">Phone Number</td>
+            <td><input class="form-control" name='reg_phone3'></td>
+        </tr>
+    </table>
+    <br>
+    <button class="btn btn-outline-info" type='submit'>Submit Data</button>
 </form>
